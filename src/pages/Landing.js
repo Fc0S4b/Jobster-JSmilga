4394de4a159111c1d5933ae2f16f0a9c7bc24d62
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-04 00:27:14 -0400

    landing page structure

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,28 @@ import React from 'react';
 import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
 const Landing = () => {
-  return <h1>Landing page</h1>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+            aliquid, aperiam libero quas sequi maiores soluta! Commodi tempore
+            excepturi placeat!
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
