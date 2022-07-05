b81b041e5810ebe5c4f4f149184503c437d3b116
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-04 22:19:23 -0400

    react- icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Wrapper from '../assets/wrappers/Navbar';
+import { FaHome } from 'react-icons/fa';
 const Navbar = () => {
-  return <Wrapper>Navbar</Wrapper>;
+  return (
+    <Wrapper>
+      Navbar
+      <FaHome />
+    </Wrapper>
+  );
 };
 
 export default Navbar;
