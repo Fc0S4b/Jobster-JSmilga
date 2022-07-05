281c695099c48d174654d1017d3d11652a68905e
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-04 23:36:23 -0400

    Small Sidebar - Nav Links

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 import Logo from './Logo';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleSidebar } from '../features/user/userSlice';
-
+import links from '../utils/links';
 const SmallSidebar = () => {
   const { isSidebarOpen } = useSelector((store) => store.user);
   const dispatch = useDispatch();
@@ -27,7 +27,24 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">nav links</div>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, path, id, icon } = link;
+              return (
+                <NavLink
+                  key={id}
+                  to={path}
+                  className={(isActive) => {
+                    return isActive ? 'nav-link active' : 'nav-link';
+                  }}
+                  onClick={toggle}
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
         </div>
       </div>
     </Wrapper>
