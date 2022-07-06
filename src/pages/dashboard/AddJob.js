e41d39aeb834f93bd3ee17e3a4e55534c8505262
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-05 22:27:46 -0400

    ClearValues Reducer

--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -3,7 +3,7 @@ import { FormRow, FormRowSelect } from '../../components';
 import Wrapper from '../../assets/wrappers/DashboardFormPage';
 import { useSelector, useDispatch, createDispatchHook } from 'react-redux';
 import { toast } from 'react-toastify';
-import { handleChange } from '../../features/job/jobSlice';
+import { handleChange, clearValues } from '../../features/job/jobSlice';
 const AddJob = () => {
   const {
     isLoading,
@@ -79,7 +79,7 @@ const AddJob = () => {
             <button
               type="button"
               className="btn btn-block clear-btn"
-              onClick={() => console.log('clear values')}
+              onClick={() => dispatch(clearValues())}
             >
               clear
             </button>
