560c04826c44d351f0f212e8273f3f9b1cab5dff
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-06 01:31:46 -0400

    AllJobs Page structure

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,16 @@ import Navbar from './Navbar';
 import SmallSidebar from './SmallSidebar';
 import BigSidebar from './BigSidebar';
 import FormRowSelect from './FormRowSelect';
+import SearchContainer from './SearchContainer';
+import JobsContainer from './JobsContainer';
 
-export { Logo, FormRow, BigSidebar, SmallSidebar, Navbar, FormRowSelect };
+export {
+  Logo,
+  FormRow,
+  BigSidebar,
+  SmallSidebar,
+  Navbar,
+  FormRowSelect,
+  JobsContainer,
+  SearchContainer,
+};
