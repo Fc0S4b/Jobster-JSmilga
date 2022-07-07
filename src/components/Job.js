fe4675323c246d6e4181ecc93c37d4d9e81b8839
Fc0S4b <fcoj.sabugo@gmail.com>
2022-07-06 22:28:04 -0400

    Job info component

--- src/components/Job.js
+++ src/components/Job.js
@@ -3,7 +3,7 @@ import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from 'react-icons/fa';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Job';
-
+import JobInfo from './JobInfo';
 const Job = ({
   _id,
   position,
@@ -14,6 +14,8 @@ const Job = ({
   status,
 }) => {
   const dispatch = useDispatch();
+
+  const date = createdAt;
 
   return (
     <Wrapper>
@@ -26,7 +28,9 @@ const Job = ({
       </header>
       <div className="content">
         <div className="content-center">
-          <h4>more content</h4>
+          <JobInfo icon={<FaLocationArrow />} text={jobLocation} />
+          <JobInfo icon={<FaCalendarAlt />} text={date} />
+          <JobInfo icon={<FaBriefcase />} text={jobType} />
           <div className={`status ${status}`}>{status}</div>
         </div>
         <footer>
